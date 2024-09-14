cc71225b43dd360cf942fdab29b7d21c64a79ce0
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2024-09-14 22:04:17 +0200

    continuing on badge board

--- components/badge-board.tsx
+++ components/badge-board.tsx
@@ -3,21 +3,21 @@ import Image from "next/image"
 
 export default function BadgeBoard() {
     return (
-        <div className="">
+        <div className={classes["badge-container"]}>
             <div>
-                <Image />
+                <Image width={20} height={20} src="/images/badges/trustly.avif" />
                 <span>Trustly</span>
             </div>
             <div>
-                <Image />
+                <Image width={20} height={20} src="/images/badges/swish.avif"/>
                 <span> Swish</span>
             </div>
             <div>
-                <Image />
+                <Image width={20} height={20} src="/images/badges/bankid.avif"/>
                 <span>BankID</span>
             </div>
             <div>
-                <Image />
+                <Image width={20} height={20} src="/images/badges/spelinspektionen.avif"/>
                 <span>Licens</span>
             </div>
         </div>
